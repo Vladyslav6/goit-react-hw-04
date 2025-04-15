58f112bb89bb9177a38c88490b29cb77e2170dd3
Vladyslav6 <vlad51284@gmail.com>
2025-04-15 13:24:18 +0300

    apiFix

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const KeyAxios = "TgX6vCr7P6VbVXRztS7x-BCKsz_x8JgBvktxAHTVZPc";
 export const fetchData = async (query, page, signal) => {
   const response = await axios.get(
-    `https://api.unsplash.com/search/photos/?client_id=${KeyAxios}&per_page=5&queryy=${query}&page=${page}`,
+    `https://api.unsplash.com/search/photos/?client_id=${KeyAxios}&per_page=5&query=${query}&page=${page}`,
     { signal }
   );
   return response.data;
